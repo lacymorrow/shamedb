86a2aca5ad3381ec26c2312f4b972a3ee2d95f58
Lacy Morrow <me@lacymorrow.com>
2022-01-19 15:42:42 -0800

    tweak

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import Status from '../components/auth/AuthStatus';
 import { Meta } from '../components/Meta';
 import VideoText from '../components/VideoText';
 import { BigTitle, TextWrapper } from '../styles';
-import Layout from '../templates/Home';
 import config from '../utils/config';
 import { generateRandom } from '../utils/utils';
 
@@ -27,14 +26,12 @@ const Index = () => {
   }, []);
 
   return (
-    <Layout
-      meta={
-        <Meta
-          title={`${config.title}: ${config.tagline}`}
-          description={config.description}
-        />
-      }
-    >
+    <div className="antialiased w-full min-h-screen px-1 py-16 text-center flex items-center flex-col">
+      <Meta
+        title={`${config.title}: ${config.tagline}`}
+        description={config.description}
+      />
+
       <Status />
 
       <TextWrapper className="hidden md:block">
@@ -60,12 +57,20 @@ const Index = () => {
         {config.title}
       </BigTitle>
 
+      <p className="font-bold text-2xl mb-12 lowercase">
+        Aerial cinematography {/* drone productions */}{' '}
+        <span className="lowercase">with</span>{' '}
+        <span className="font-extrabold text-white bg-gray-900 p-1 uppercase">
+          Impact
+        </span>
+      </p>
+
       <Link href="/contact">
         <a className="font-bold text-4xl border-4 border-gray-900 transition text-gray-900 hover:text-white hover:bg-gray-900 p-6">
-          Contact
+          Interested?
         </a>
       </Link>
-    </Layout>
+    </div>
   );
 };
 
